c84dd5c93bf91feb4a0289fc0e9c3b68b8c1ab7c
Alexey Krupnov <alexey.krupnov@gmail.com>
2026-06-06 16:33:13 +0300

    war packaging for Tomcat deployment

--- src/main/java/com/example/rest/RestApplication.java
+++ src/main/java/com/example/rest/RestApplication.java
@@ -2,9 +2,10 @@ package com.example.rest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class RestApplication {
+public class RestApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(RestApplication.class, args);
