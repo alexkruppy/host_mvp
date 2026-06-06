df6541cd63495bb42e203a721d9a8250439e7e0a
Alexey Krupnov <alexey.krupnov@gmail.com>
2026-06-06 18:22:36 +0300

    all docker config in pom.xml, removed jib, using exec-maven-plugin with -H flag

--- deploy.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-setlocal
-if "%DOCKER_HOST%"=="" set "DOCKER_HOST=tcp://192.168.1.69:2375"
-docker stop rest-app 2>nul
-docker rm rest-app 2>nul
-docker run -d --name rest-app --restart unless-stopped -p 8080:8080 rest-app
-endlocal
