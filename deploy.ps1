d50727bdb2e903cde948e31a0053598add97ab81
Alexey Krupnov <alexey.krupnov@gmail.com>
2026-06-07 00:29:11 +0300

    update deploy.ps1 to use plink/pscp with password auth

--- deploy.ps1
+++ deploy.ps1
@@ -1,8 +1,11 @@
 $VM_USER = "vboxuser"
 $VM_HOST = "192.168.1.69"
+$VM_PASS = "bloodmaniac"
 $VM_DIR = "/home/$VM_USER/host_mvp"
 $IMAGE_NAME = "spring_rest-app:latest"
 $TAR_FILE = "host_mvp-image.tar"
+$PLINK = ".\plink.exe"
+$PSCP = ".\pscp.exe"
 
 Write-Host "=== Building Docker image ==="
 docker compose build
@@ -13,20 +16,16 @@ docker save $IMAGE_NAME -o $TAR_FILE
 if ($LASTEXITCODE -ne 0) { throw "Docker save failed" }
 
 Write-Host "=== Creating directory on VM ==="
-ssh "${VM_USER}@${VM_HOST}" "mkdir -p $VM_DIR"
-if ($LASTEXITCODE -ne 0) { throw "SSH mkdir failed" }
+& $PLINK -ssh -pw $VM_PASS "${VM_USER}@${VM_HOST}" "mkdir -p $VM_DIR"
+if ($LASTEXITCODE -ne 0) { throw "Failed to create directory on VM" }
 
 Write-Host "=== Copying files to VM ==="
-scp $TAR_FILE "${VM_USER}@${VM_HOST}:$VM_DIR/"
-scp docker-compose.yml "${VM_USER}@${VM_HOST}:$VM_DIR/"
+& $PSCP -pw $VM_PASS $TAR_FILE "${VM_USER}@${VM_HOST}:$VM_DIR/"
+& $PSCP -pw $VM_PASS docker-compose.yml "${VM_USER}@${VM_HOST}:$VM_DIR/"
 if ($LASTEXITCODE -ne 0) { throw "SCP failed" }
 
 Write-Host "=== Loading image and starting containers on VM ==="
-ssh "${VM_USER}@${VM_HOST}" @"
-cd $VM_DIR
-docker load -i $TAR_FILE
-docker compose up -d
-"@
+& $PLINK -ssh -pw $VM_PASS "${VM_USER}@${VM_HOST}" "cd $VM_DIR && docker load -i $TAR_FILE && docker compose up -d"
 if ($LASTEXITCODE -ne 0) { throw "Deploy on VM failed" }
 
 Write-Host "=== Cleaning up ==="
